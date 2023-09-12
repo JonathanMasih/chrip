7d0333b0c2a40a9d866afd91d3cf020d616728d9
Jonathan <jonathanmasih42@gmail.com>
2023-09-11 20:01:39 -0400

    server side profile props fixed

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,15 +1,15 @@
 
-import type { GetStaticProps, NextPage } from "next";
+import type { GetServerSidePropsContext, GetStaticProps, InferGetServerSidePropsType, NextPage } from "next";
 import Head from "next/head";
 import { api } from "y/utils/api";
 import { appRouter } from "y/server/api/root";
 import { prisma } from "y/server/db";
 import superjson from "superjson";
 import { PageLayout } from "y/components/layout";
-import { createServerSideHelpers } from "@trpc/react-query/dist/server/ssgProxy";
+import { createServerSideHelpers } from '@trpc/react-query/server';
 
-const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
-
+const ProfilePage = (  props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { username } = props;
   const { data } = api.profile.getUserByUsername.useQuery({
     username,
   });
@@ -27,30 +27,58 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   );
 }
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const ssg = createServerSideHelpers ({
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   const ssg = createServerSideHelpers ({
+//     router: appRouter,
+//     ctx: {prisma , userId: null},
+//     transformer: superjson
+//     });
+//   const slug = context.params?.slug;
+
+//   if (typeof slug !== "string") throw new Error("No slug");
+
+//   const username = slug.replace("@", "");
+
+//   await ssg.profile.getUserByUsername.prefetch({ username: slug });
+
+//   return {
+//     props: {
+//       trpcState: ssg.dehydrate(),
+//       username,
+//     },
+//   }
+// }
+
+export async function getServerSideProps(
+  context: GetServerSidePropsContext<{ slug: string }>,
+) {
+  const helpers = createServerSideHelpers({
     router: appRouter,
     ctx: {prisma , userId: null},
-    transformer: superjson
-    });
-  const slug = context.params?.slug;
+    transformer: superjson,
+  });
+   const slug = context.params?.slug;
 
-  if (typeof slug !== "string") throw new Error("No slug");
+     if (typeof slug !== "string") throw new Error("No slug");
 
   const username = slug.replace("@", "");
-
-  await ssg.profile.getUserByUsername.prefetch({ username: slug });
-
+  /*
+   * Prefetching the `post.byId` query.
+   * `prefetch` does not return the result and never throws - if you need that behavior, use `fetch` instead.
+   */
+  await helpers.profile.getUserByUsername.prefetch({ username});
+  // Make sure to return { props: { trpcState: helpers.dehydrate() } }
   return {
     props: {
-      trpcState: ssg.dehydrate(),
+      trpcState: helpers.dehydrate(),
       username,
     },
-  }
+  };
 }
 
-export const getStaticPaths = () => {
-  return { path: [], fallback: "blocking" }
-}
+
+// export const getStaticPaths = () => {
+//   return { path: [], fallback: "blocking" }
+// }
 
 export default ProfilePage;
